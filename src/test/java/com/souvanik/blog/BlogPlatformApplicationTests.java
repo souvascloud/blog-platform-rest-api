c46b91c9b896c99ca93821f675df116b8e82fbf2
unknown <devsouva@gmail.com>
2025-12-31 21:39:05 +0530

    flyway disable for test

--- src/test/java/com/souvanik/blog/BlogPlatformApplicationTests.java
+++ src/test/java/com/souvanik/blog/BlogPlatformApplicationTests.java
@@ -2,8 +2,10 @@ package com.souvanik.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlogPlatformApplicationTests {
 
 	@Test
