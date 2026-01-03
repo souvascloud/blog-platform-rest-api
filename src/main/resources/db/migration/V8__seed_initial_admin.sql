2058d7e198e5f9dbdf12511d1c2b00b6a316e379
unknown <devsouva@gmail.com>
2026-01-03 11:34:01 +0530

    updated .yml for docker compose

--- src/main/resources/db/migration/V8__seed_initial_admin.sql
+++ src/main/resources/db/migration/V8__seed_initial_admin.sql
@@ -12,7 +12,7 @@ VALUES (
            gen_random_uuid(),
            'admin',
            'admin@blog.com',
-           '$2a$10$McYV6gaFjP95LRrzdIM52OIhbcyKqxGGeUfZpWTNTMNQr/UCQfKoK',
+           '$2a$10$McYV6gaFjP95LRrzdIM52OIhbcyKqxGGeUfZpWTNTMNQr/UCQfKoK', -- Admin@123
            'ADMIN',
            'ACTIVE',
            now()
