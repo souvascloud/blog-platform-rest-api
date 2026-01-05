ef54a3871ec3717c45c5bf8e4656d7fb74149bb2
Souvanik Saha <souvascloud@gmail.com>
2026-01-05 22:21:41 +0530

    Change UUID generation method in SQL seed script

--- src/main/resources/db/migration/V8__seed_initial_admin.sql
+++ src/main/resources/db/migration/V8__seed_initial_admin.sql
@@ -8,7 +8,7 @@ INSERT INTO users (
     status,
     created_at
 )
-VALUES(      RANDOM_UUID(),
+VALUES(       gen_random_uuid(),
            'admin',
            'admin@blog.com',
            '$2a$10$McYV6gaFjP95LRrzdIM52OIhbcyKqxGGeUfZpWTNTMNQr/UCQfKoK', -- Admin@123
