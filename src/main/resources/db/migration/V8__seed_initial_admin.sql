8fa2da1cf7003acfc89676608ba431a20586e2c3
unknown <devsouva@gmail.com>
2026-01-05 21:50:29 +0530

    fix flyway migration for test

--- src/main/resources/db/migration/V8__seed_initial_admin.sql
+++ src/main/resources/db/migration/V8__seed_initial_admin.sql
@@ -9,7 +9,7 @@ INSERT INTO users (
     created_at
 )
 VALUES (
-           gen_random_uuid(),
+            RANDOM_UUID(),
            'admin',
            'admin@blog.com',
            '$2a$10$McYV6gaFjP95LRrzdIM52OIhbcyKqxGGeUfZpWTNTMNQr/UCQfKoK', -- Admin@123
